9534ba9af3cd7060d1ad3b8988c1da10b1fe12bc
mylxsw <mylxsw@aicode.cc>
2024-02-15 17:08:28 +0800

    update

--- build.sh
+++ build.sh
@@ -2,5 +2,4 @@
 
 VERSION=1.0.3
 
-docker buildx build --platform=linux/amd64 . -t mylxsw/one-api:$VERSION
-docker push mylxsw/one-api:$VERSION
+docker buildx build --platform=linux/amd64 . -t mylxsw/one-api:$VERSION --push
